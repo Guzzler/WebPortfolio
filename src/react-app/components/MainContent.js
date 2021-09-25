281ee8cdbfbe769a880aedfed0627fc9d371062a
Guzzler <sharangpai123@gmail.com>
2021-09-25 22:34:52 +0530

    basic logo

--- src/react-app/components/MainContent.js
+++ src/react-app/components/MainContent.js
@@ -1,35 +1,23 @@
-import React from 'react'
-import { Row } from 'antd'
-import {
-  Route,
-  Switch
-} from 'react-router-dom'
-import posed, { PoseGroup } from 'react-pose'
+import React from "react";
+import { Row } from "antd";
+import { Route, Switch } from "react-router-dom";
 
-import BaseContent from '../components/main-content/BaseContent'
-import PageNotFound from '../components/common/error-pages/PageNotFound'
+import BaseContent from "../components/main-content/BaseContent";
+import PageNotFound from "../components/common/error-pages/PageNotFound";
 
-const RouteContainer = posed.div({
-  enter: { opacity: 1, delay: 300, beforeChildren: true },
-  exit: { opacity: 0 }
-})
-
-function MainContent () {
+function MainContent() {
   return (
     <Route
       render={({ location }) => (
-        <Row className='overflow-auto display-block'>
-          <PoseGroup>
-            <RouteContainer key={999999}>
-              <Switch location={location}>
-                <Route path="/" component={BaseContent} key="base" />
-                <Route render={() => <PageNotFound />} key="notFound" />
-              </Switch>
-            </RouteContainer>
-          </PoseGroup>
+        <Row className="overflow-auto display-block">
+          <Switch location={location}>
+            <Route path="/" component={BaseContent} key="base" />
+            <Route render={() => <PageNotFound />} key="notFound" />
+          </Switch>
         </Row>
-      )} />
-  )
+      )}
+    />
+  );
 }
 
-export default MainContent
+export default MainContent;
--- src/react-app/components/main-content/BaseContent.js
+++ src/react-app/components/main-content/BaseContent.js
@@ -1,30 +1,46 @@
-import React from 'react'
-import { Row, Col } from 'antd'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React from "react";
+import { Row, Col } from "antd";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import CircleType from "circletype";
+import piImage from "../../../assets/images/pi-image.jpg";
+import './BaseContent.css';
 
-class BaseContent extends React.Component {
-  render() {
-    return (
-      <Row className='padding--sides width-100 height-100 background-white'>
-        <Col span={24} className='center' >
-          This is my website
-        </Col>
-      </Row>
-    )
-  }
-}
+const BaseContent = () => {
+  React.useEffect(() => {
+    const circleType = new CircleType(
+      document.getElementById("test-ele")
+    );
+    circleType.radius(130);
+  });
+  return (
+    <Row className="width-100 height-100 background-white">
+      <Col span={4} className="center left-column background-blue">
+        <div className="pi-logo-entire">
+          <span id="test-ele" className="f18 source-code-font text-white">
+            Sharang{" "}
+          </span>
+          <img src={piImage} alt="pi" className='pi-logo-image' />
+        </div>
+      </Col>
+      <Col span={10} className="resume-first-col">
+        test
+      </Col>
+      <Col span={10} className="resume-second-col">
+        Test
+      </Col>
+    </Row>
+  );
+};
 
 BaseContent.propTypes = {
   base: PropTypes.object.isRequired,
-}
+};
 
 const mapStateToProps = ({ base }) => {
   return {
-    base
-  }
-}
+    base,
+  };
+};
 
-export default connect(
-  mapStateToProps, {
-})(BaseContent)
+export default connect(mapStateToProps, {})(BaseContent);
