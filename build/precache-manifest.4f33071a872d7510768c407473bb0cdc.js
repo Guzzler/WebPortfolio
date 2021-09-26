86e8c2c223945ceadde7234c99f05570bb4a7004
Guzzler <sharangpai123@gmail.com>
2021-09-26 17:25:42 +0530

    push correct build

---
+++ build/precache-manifest.4f33071a872d7510768c407473bb0cdc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d05df326e44c9570097947bcba6a25ba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "96c06817804b445a2415",
+    "url": "./static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "c71de4b4715973ca2f99",
+    "url": "./static/css/main.3f78182a.chunk.css"
+  },
+  {
+    "revision": "96c06817804b445a2415",
+    "url": "./static/js/2.3adde08c.chunk.js"
+  },
+  {
+    "revision": "d0167a5d374b6dbae81a0a640367df9d",
+    "url": "./static/js/2.3adde08c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c71de4b4715973ca2f99",
+    "url": "./static/js/main.c3c0325b.chunk.js"
+  },
+  {
+    "revision": "86f2bb3097d42d6c94ba",
+    "url": "./static/js/runtime-main.be9ee3f7.js"
+  },
+  {
+    "revision": "f352e4548905c64f588209f37a481243",
+    "url": "./static/media/pi-image.f352e454.jpg"
+  }
+]);
